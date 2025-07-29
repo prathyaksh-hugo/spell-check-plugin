bc95d95c9660e4127f1f7d5ab69bb5355c3c031e
prathyaksh-hugo <prathyaksh.malligari@hugohub.com>
2025-07-29 12:11:24 +0530

    extracted-text from the figma page

--- code.ts
+++ code.ts
@@ -3,9 +3,28 @@ if (figma.editorType === 'figma') {
 
   figma.showUI(__html__);
 
+  figma.ui.onmessage = async (msg: {type: string}) => {
+
+    if(msg.type === 'spell-check'){
+      const textNodes = figma.currentPage.findAllWithCriteria({
+        types: ['TEXT']
+      })
+
+      const allTextContent: string [] = [] ;
+      for(const node of textNodes){
+        allTextContent.push(node.characters)
+      }
+      console.log('All extracted text:', allTextContent);
+      console.log(`Found and extracted text from ${allTextContent.length} text nodes.`);
+
+  
+  figma.notify(`Found ${allTextContent.length} text layers on the page.`);
+  figma.ui.postMessage({ type: 'all-text-data', payload: allTextContent });
+
+    }
+  }
 
 }
-
 
 if (figma.editorType === 'figjam') {
 
